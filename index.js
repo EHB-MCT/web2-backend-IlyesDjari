0fae2a62ad9b0cd14d23fe56c2e06d81a739acaf
IlyesDjari <ilyes.djari@student.ehb.be>
2021-11-27 00:04:56 +0100

    Succesfully connected to mongoDB Database

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+// Load all environment variables from env file
+require('dotenv').config();
+
+// To use express library
 const express = require('express');
+const req = require('express/lib/request');
+// Create app variable to configure server
 const app = express();
+// To use mongoose library
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.URL);
+const db = mongoose.connection;
+
+//Log error if connection fails
+db.on('error', (error) => console.error(error));
+
+// Conseole log a succesfull connection to DB
+db.once('open', () => console.log('Succesfully connected to Database'));
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
 res.send('Hello Ilyes i <3');
 });
 
+app.get('/OTO', (req, res) => {
+    res.send(["damso", "hamza", "booba"]);
+    });
 
-// Preparing for hosting envirement, support of dynamic port, if none => use 3000
+app.get('/OTO/:ID', (req, res) => {
+    res.send(req.params.ID);
+    });
+
+// Preparing for hosting environment, support of dynamic port, if none => use 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
