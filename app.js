3a6db46c486fb73904802df68e6e961b50e00a1b
IlyesDjari <ilyes.djari@student.ehb.be>
2021-12-14 14:42:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,21 @@ const mongoose = require('mongoose');
 // Create app variable to configure server
 const app = express();
 
+const URL = process.env.URL;
+
 /*CONNECTION TO MY MONGODB DATABASE*/
-mongoose.connect(process.env.URL);
+mongoose.connect(URL);
 const db = mongoose.connection;
 //Log error if connection fails
 db.on('error', (error) => console.error(error));
 // Conseole log a succesfull connection to DB
 db.once('open', () => console.log('Succesfully connected to Database'));
 
+
+
+
 // Accept JSON as a body instead of POST element
 app.use(express.json());
-
 app.get("/", function (req, res) {
   res.send("Working perfectly ilyes");
 })
