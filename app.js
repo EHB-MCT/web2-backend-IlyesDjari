fb5c0fbacec12be429406ebccb9ed106925c2d40
IlyesDjari <ilyes.djari@student.ehb.be>
2021-12-14 14:47:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // Load all environment variables from env file
 require('dotenv').config();
+const fs = require('fs/promises');
+const bodyParser = require('body-parser');
 // To use express library
 const express = require('express');
 const mongoose = require('mongoose');
 
 // Create app variable to configure server
 const app = express();
-
+const port = process.env.PORT;
 const URL = process.env.URL;
 
 /*CONNECTION TO MY MONGODB DATABASE*/
@@ -21,7 +23,7 @@ db.once('open', () => console.log('Succesfully connected to Database'));
 
 
 // Accept JSON as a body instead of POST element
-app.use(express.json());
+app.use(bodyParser.json());
 app.get("/", function (req, res) {
   res.send("Working perfectly ilyes");
 })
