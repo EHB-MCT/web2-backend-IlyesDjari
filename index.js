7e4eeb8bb1f9a1dc0f892078de4fe7a7e8675db7
IlyesDjari <ilyes.djari@student.ehb.be>
2021-12-14 23:45:30 +0100

    Cors error handeling

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ const PROFILE = "https://api.spotify.com/v1/me";
 const CHOICES = [];
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 
 app.get('/connect', function routeHandler(req, res) {
