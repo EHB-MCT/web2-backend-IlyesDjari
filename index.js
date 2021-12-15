33625e8f044f0b2d9e629c4b2551b9f8d00ddff6
IlyesDjari <ilyes.djari@student.ehb.be>
2021-12-15 14:45:43 +0100

    Get code after authorization

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/connect', function routeHandler(req, res, next) {
 
   app.get('/getcode', function routeHandler(req, res, next) {
 
-  res.send({"data": code});
+  res.send({"data": body});
   });
 
 
